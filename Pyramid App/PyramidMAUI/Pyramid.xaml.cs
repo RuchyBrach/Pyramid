067c4e5799884198a800d3024ccef59da9237a19
ruchybrach <brach4a@gmail.com>
2026-05-27 15:15:22 -0400

    hw 36

--- Pyramid App/PyramidMAUI/Pyramid.xaml.cs
+++ Pyramid App/PyramidMAUI/Pyramid.xaml.cs
@@ -1,97 +1,97 @@
-//using PyramidSystem;
+using PyramidSystem;
 
 namespace PyramidMAUI;
 
 public partial class Pyramid : ContentPage
 {
-    //Game game = new();
-    //List<Entry> lstentries;
-    //List<List<Entry>> lstRows;
-    //List<Label> lstblocks;
+    Game game = new();
+    List<Entry> lstentries;
+    List<List<Entry>> lstRows;
+    List<Label> lstblocks;
 
     public Pyramid()
     {
         InitializeComponent();
-        //BindingContext = game;
-        //lstentries = new() { txtRow11, txtRow21, txtRow22, txtRow31, txtRow32, txtRow33, txtRow41, txtRow42, txtRow43, txtRow44, txtRow51, txtRow52, txtRow53, txtRow54, txtRow55 };
+        BindingContext = game;
+        lstentries = new() { txtRow11, txtRow21, txtRow22, txtRow31, txtRow32, txtRow33, txtRow41, txtRow42, txtRow43, txtRow44, txtRow51, txtRow52, txtRow53, txtRow54, txtRow55 };
 
-        //lstRows = new()
-        //    {
-        //    new() { txtRow11 },
-        //    new() { txtRow21, txtRow22 },
-        //    new() { txtRow31, txtRow32, txtRow33 },
-        //    new() { txtRow41, txtRow42, txtRow43, txtRow44 },
-        //    new() { txtRow51, txtRow52, txtRow53, txtRow54, txtRow55 },
-        //    };
+        lstRows = new()
+            {
+            new() { txtRow11 },
+            new() { txtRow21, txtRow22 },
+            new() { txtRow31, txtRow32, txtRow33 },
+            new() { txtRow41, txtRow42, txtRow43, txtRow44 },
+            new() { txtRow51, txtRow52, txtRow53, txtRow54, txtRow55 },
+            };
 
-        //lstblocks = new() { lblBlock3, lblBlock2, lblBlock1 };
-        //lstentries.ForEach(entry => entry.TextChanged += Entry_TextChanged);
-        //this.Loaded += FrmPyramid_Loaded;
+        lstblocks = new() { lblBlock3, lblBlock2, lblBlock1 };
+        lstentries.ForEach(entry => entry.TextChanged += Entry_TextChanged);
+        this.Loaded += FrmPyramid_Loaded;
     }
 
-    
-
-    //private void FrmPyramid_Loaded(object? sender, EventArgs e)
-    //{
-    //    StartGame();
-    //}
 
 
-    //private void StartGame()
-    //{
-    //    game.StartGame();
-    //    lstRows[game.CurrentRowIndex].First().Focus();
-    //}
+    private void FrmPyramid_Loaded(object? sender, EventArgs e)
+    {
+        StartGame();
+    }
 
-    //private async void Entry_TextChanged(object sender, TextChangedEventArgs e)
-    //{
-    //    if (sender is Entry entry)
-    //    {
 
-    //        int row = lstRows.FindIndex(r => r.Contains(entry));
+    private void StartGame()
+    {
+        game.StartGame();
+        lstRows[game.CurrentRowIndex].First().Focus();
+    }
 
-    //        if (game.UpdateRowProgress(row) == false)
-    //        {
-    //            lstRows[row].ForEach(e => e.IsEnabled = true);
-    //            if (game.Message != "")
-    //            {
-    //                await DisplayAlert("Pyramid", game.Message, "OK");
-    //            }
-    //            if (game.RemainingAttempts == 0)
-    //            {
-    //                StartGame();
-    //            }
+    private async void Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (sender is Entry entry)
+        {
 
-    //            else
-    //            {
-    //                lstRows[row].First().Focus();
-    //            }
-    //            return;
-    //        }
+            int row = lstRows.FindIndex(r => r.Contains(entry));
 
-    //        int index = lstentries.FindIndex(i => i == entry);
-    //        int nextindex = index + 1;
-    //        if (nextindex < lstentries.Count && lstentries[index].Text != "")
-    //        {
-    //            Entry nextentry = lstentries[nextindex];
-    //            nextentry.IsEnabled = true;
-    //            nextentry.Focus();
+            if (game.UpdateRowProgress(row) == false)
+            {
+                lstRows[row].ForEach(e => e.IsEnabled = true);
+                if (game.Message != "")
+                {
+                    await DisplayAlert("Pyramid", game.Message, "OK");
+                }
+                if (game.RemainingAttempts == 0)
+                {
+                    StartGame();
+                }
 
-    //            int nextRow = lstRows.FindIndex(r => r.Contains(nextentry));
-    //            int nextCol = lstRows[nextRow].IndexOf(nextentry);
+                else
+                {
+                    lstRows[row].First().Focus();
+                }
+                return;
+            }
 
-    //            if (nextCol == 0 && nextRow > 0)
-    //            {
-    //                lstRows[nextRow - 1].ForEach(e => e.IsEnabled = false);
-    //            }
-    //        }
-    //    }
-    //}
+            int index = lstentries.FindIndex(i => i == entry);
+            int nextindex = index + 1;
+            if (nextindex < lstentries.Count && lstentries[index].Text != "")
+            {
+                Entry nextentry = lstentries[nextindex];
+                nextentry.IsEnabled = true;
+                nextentry.Focus();
 
-    //private async void btnRules_Clicked(object sender, EventArgs e)
-    //{
-    //    await DisplayAlert("Rules", game.Rules, "OK");
-    //    lstentries.FirstOrDefault(entry => entry.Text == "")?.Focus();
-    //}
+                int nextRow = lstRows.FindIndex(r => r.Contains(nextentry));
+                int nextCol = lstRows[nextRow].IndexOf(nextentry);
+
+                if (nextCol == 0 && nextRow > 0)
+                {
+                    lstRows[nextRow - 1].ForEach(e => e.IsEnabled = false);
+                }
+            }
+        }
+    }
+
+    private async void btnRules_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Rules", game.Rules, "OK");
+        lstentries.FirstOrDefault(entry => entry.Text == "")?.Focus();
+    }
 
 }
